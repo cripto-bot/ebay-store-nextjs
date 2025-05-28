5189bdac3014410466df060d4ea3ce6ddce7fe4e
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-05-28 07:57:01 -0300

    Delete pages/api directory

--- pages/api/get-ebay-token.js
+++
@@ -1,7 +0,0 @@
-// lib/ebayConfig.js
-export const EBAY_PROD_CLIENT_ID = process.env.EBAY_PROD_CLIENT_ID;
-export const EBAY_PROD_CLIENT_SECRET = process.env.EBAY_PROD_CLIENT_SECRET;
-export const EBAY_PROD_TOKEN_URL = "https://api.ebay.com/identity/v1/oauth2/token";
-export const EBAY_PROD_BROWSE_API_URL = "https://api.ebay.com/buy/browse/v1/item_summary/search";
-export const REQUESTED_PROD_SCOPES = ["https://api.ebay.com/oauth/api_scope"];
-export const SCOPES_PROD_STRING = REQUESTED_PROD_SCOPES.join(" ");
